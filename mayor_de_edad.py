26947307319834e11b962d6be9de1804bad6141a
KevinCamilo RR <Kevincamilorr@gmail.com>
2025-04-14 20:57:44 -0500

    "Ejercicio de condicionales: verificar si una persona es mayor de edad"

---
+++ mayor_de_edad.py
@@ -0,0 +1,8 @@
+# e pequeño programa le pide al usuario su edad
+# y le dice si es mayor o menor de edad.
+
+edad = int(input("¿Cuantos años tienes? "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")

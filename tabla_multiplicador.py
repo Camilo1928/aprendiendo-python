ba3a9d8d10aadb03c68e52e647dec3c1c3256b2b
KevinCamilo RR <Kevincamilorr@gmail.com>
2025-04-14 21:30:07 -0500

    Eliminado archivo incorrecto

---
+++ tabla_multiplicador.py
@@ -0,0 +1,14 @@
+
+numero = int(input("Ingresa un nuemro: "))
+print(f"Table de multiplicar del {numero}:")
+
+for i in range (1, 11):
+    print(f"{numero} x {i} = {numero * i}")
+
+
+
+numero = int(input("Ingresa un numero: "))
+print(f"Table de multuplicar del {numero}:")
+
+for i in range (1, 11):
+    print(f"{numero} x {i} = {numero * i}")

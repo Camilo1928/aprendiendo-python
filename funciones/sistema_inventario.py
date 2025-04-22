12665eb87cdd7c346116176cfcc4418c40bb29e0
KevinCamilo RR <Kevincamilorr@gmail.com>
2025-04-21 20:27:31 -0500

    Tarea pendiente

---
+++ funciones/sistema_inventario.py
@@ -0,0 +1,18 @@
+print('*** Sistema d inventarios (con funciones)***')
+print('''--- Menu ---
+        1. Mostrar inventario
+        2. Agregar nueva producto
+        3. Buscar producto por Id
+        4. Salir
+''')
+
+
+
+opcion = int(input('Proporciona una opcion (1-4): '))
+
+def inventario():
+    if opcion == 1:
+        return inventario
+    elif opcion == 2:
+        return
+        agregar_inventario = input('Agregar nueva producto')

8f4390ecb31502302d4c7cf3266eaadf80f4f4ae
KevinCamilo RR <Kevincamilorr@gmail.com>
2025-04-26 08:56:23 -0500

    clss donde se incluye la direccion de memoria de un objeto al inpromirlo

--- Clases_Objetos/constructores.py
+++ Clases_Objetos/constructores.py
@@ -12,9 +12,11 @@ class Carro:
         self.modelo = modelo
 
     def mostrar_carro(self):
-        print(f'''Persona:
-            Marca: {self.marca}
-            Modelo: {self.modelo}''')
+        print(f'''Carro:
+        Marca: {self.marca}
+        Modelo: {self.modelo}''')
+        print(f'Dir. men self: {id(self)}')
+        print(f'Dir. men hex self: {hex(id(self))}')
         
 if __name__ == '__main__':
         carro1 = Carro('Mazda', 2025)
@@ -23,4 +25,14 @@ if __name__ == '__main__':
         carro1.mostrar_carro()
         # diferencia importante; desde el momneto en que estamos creando nuestro objeto, 
         # se estan inicializando los atributos de nuestra class
+        print(f'Dir. men carro1: {id(carro1)}')
+        print(f'Dir. men hex carro1: {hex(id(carro1))}')
+
+
         carro2 = Carro('Hiundai', 2024) 
+        carro2.mostrar_carro()
+        print(f'Dir. men carro2: {id(carro2)}')
+        print(f'Dir. men hex carro2: {hex(id(carro2))}')
+
+def invertir_palabra(palabra):
+     pass

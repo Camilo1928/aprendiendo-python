ac6a89dcb1e68bfc76e8fffc380455fb142dba4d
KevinCamilo RR <Kevincamilorr@gmail.com>
2025-05-08 08:42:13 -0500

    tenemos 2 clases, clase padre y clase hija; y los metodos de la clase padre se pueden heredar a las clases hija

---
+++ Clases_Objetos/herencias.py
@@ -0,0 +1,16 @@
+print("*** Herencias ***")
+
+class Animal:
+
+    def comer(self):
+        print("Como muchas veces al dia")
+
+    def dormir(self):
+        print("Duermo muchas horas"):
+
+class Perro(Animal):
+    def hacer_sonido(self):
+        print("Puedo ladrar")
+
+
+    
